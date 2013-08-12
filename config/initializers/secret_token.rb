4a9fbbc941aa47b317d9df780791cb02a73af24f
emahs8w/em <emahs8w@virginia.edu/em>
2013-08-12 18:50:40 +0200

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Thoughtsy::Application.config.secret_key_base = '1c68ab555af2736feb4b69eb30f77085f6667bc05dfd3ee4b788cbf7a668bf68d2e1936db383fa49b8480d8121f2448fed65b11e7a2f2bdfe5df5e73606f56b4'
