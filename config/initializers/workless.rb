09f86a10ca1888a6cb9c813ed6ca9960732971f8
emahs8w/em <emahs8w@virginia.edu/em>
2013-11-29 18:20:28 +0100

    took down workless initializer

--- config/initializers/workless.rb
+++
@@ -1,8 +0,0 @@
-# according to www.nicholas-hwang.com/s/4
-# fix bug with Rails 4; workless not scaling down dynos after running background job
-
-Delayed::Backend::ActiveRecord::Job.class_eval do
-  after_destroy "self.class.scaler.down"
-  after_create "self.class.scaler.up"
-  after_update "self.class.scaler.down"
-end
