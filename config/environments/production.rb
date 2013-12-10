f789a4f704b24bcb872759ad39976cb6ab1bb7db
emahs8w/em <emahs8w@virginia.edu/em>
2013-12-10 09:46:14 -0500

    and again

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -34,7 +34,7 @@ Thoughtsy::Application.configure do
   config.assets.enabled = true
 
   # Version of your assets, change this if you want to expire all your assets.
-  config.assets.version = '1.0'
+  config.assets.version = '1.1'
 
   # Specifies the header that your server uses for sending files.
   # config.action_dispatch.x_sendfile_header = "X-Sendfile" # for apache
