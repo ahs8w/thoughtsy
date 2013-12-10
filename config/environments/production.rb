621ce85373aae809e0bf8da20e3d0d9e3deaa160
emahs8w/em <emahs8w@virginia.edu/em>
2013-12-09 19:02:20 -0500

    relaunch cdn

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -55,7 +55,7 @@ Thoughtsy::Application.configure do
   # config.cache_store = :mem_cache_store
 
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
-  config.action_controller.asset_host = "//d3fpdqs119hhq4.cloudfront.net"
+  config.action_controller.asset_host = "//d17wnm2x9v5s96.cloudfront.net"
   # config.font_assets.origin = "https://thoughtsy.com"
 
   # Precompile additional assets.
